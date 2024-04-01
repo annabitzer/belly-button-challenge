8913e037d0b9b2e860bc7b31230682b1582f9275
Anna <anna.c.bitzer@gmail.com>
2024-04-01 18:36:08 -0500

    further troubleshooting on getting data to read in

--- Starter_Code/StarterCode/static/js/app.js
+++ Starter_Code/StarterCode/static/js/app.js
@@ -2,65 +2,79 @@
 const samples_url = "https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json"
 
 // Use d3.json to read in the data
-d3.json(samples_url).then(function(data) {
-    console.log(data); // Check if the data loaded correctly
+dataAccess = d3.json(samples_url)
 
-// save all available ids
-let ids = data.names
-console.log(ids)
-
+//populate dropdown menu and populate charts with results from an id
+function dropDownMenu() {
 //create a dropdown variable to provide the id options
-let dropDown = d3.select("#selDataset");
+    let dropDown = d3.select("#selDataset");
+// when subject id dropdown is changed, call function
+    d3.selectAll("#selDataset").on("change", optionChanged);
 
-//the following code was made using the following sources: https://stackoverflow.com/questions/20780835/putting-the-country-on-drop-down-list-using-d3-via-csv-file,
-    //https://www.tutorialsteacher.com/d3js/data-binding-in-d3js, and BootcampSpot: Xpert Learning Assistant
+    // save all available ids
+    dataAccess.then((data) => {
+    let ids = data.names
 
-//provide an array of ids to populate
-dropDown.selectAll("option").data(ids).enter().append("option")
-.attr("value", function(id) {return id; })
-.text(function(id) {return id; });
+    //the following code was made using the following sources: https://stackoverflow.com/questions/20780835/putting-the-country-on-drop-down-list-using-d3-via-csv-file,
+        //https://www.tutorialsteacher.com/d3js/data-binding-in-d3js, BootcampSpot: Xpert Learning Assistant, and help from classmate Justyn Helgeson.
 
-function optionChanged() {
-    //access the selected value from the dropdown menu
-    let choice = dropDown.property("value");
-    console.log("Selected value: ", choice);
+    //provide an array of ids to populate
+    ids.forEach(id => {
+        dropDown.append("option")
+        .property('value', id)
+        .text(id);
+        });
+
+    //select results from first id
+    let id = ids[0];
+
+    console.log(id);
+
+    //default display visuals with first selected id
+     barChart(id);
+    //bubbleChart(id);
+    //metaData(id);
+    });
 }
 
-// when subject id dropdown is changed, call function
-dropDown.on("change", optionChanged);
+//create bar chart function
+function barChart(choice) {
+    
+    //filter to only the data from the chosen id
+    let testSubject = dataAccess.filter(data.samples == choice)
 
-});
+    //sort by from biggest to smallest OTU based on sample value
+    let sortedSampleData = testSubject.sort((a, b) => b.sample_values - a.sample_values);
 
-// i need 'this' to be the chosen id value from the list of available ids
-
-//access the "sample" portion of the json
-//let sample_data = choice.samples
-
-//sort by from biggest to smallest OTU based on sample value
-//let sortedSampleData = sample_data.sort((a, b) => b.sample_values - a.sample_values);
-
-//Choose only the top 10 OTUs
-//let topTen = sortedSampleData.slice(0,10);
+    //Choose only the top 10 OTUs
+    let topTen = sortedSampleData.slice(0,10);
 
 //console log to check that the top 10 were correctly selected
-//console.log(topTen);
+    console.log(topTen);
 
 //Set up trace for bar chart
-//trace1 = {
-//    x: topTen.sample_values,
- //   y: topTen.otu_ids,
-   // type: "bar",
-    //orientation: "h"
-//};
+    trace1 = {
+       x: topTen.sample_values,
+       y: topTen.otu_ids,
+       type: "bar",
+       orientation: "h"
+             };
 
-//let barData = [trace1];
+    let barData = [trace1];
 
-//Plotly.newPlot("bar", barData);
+    Plotly.updatePlot("bar", barData);
+    }
 
-//console.log(id)
+//create bubble chart function
 
-    //}
+//create metadata function
 
-//});
+//create optionChanged to run whenever the chosen id is changed
+    function optionChanged(choice) {
+    barChart(choice);
+    //bubbleChart(choice);
+    //metaData(choice);
+    }
+
 
 
